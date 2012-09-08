d20b9e2e167805b5e1a54b0eaf8f369eb295bf21
Dmitriy Shestavin <dimatwl@gmail.com>
2012-09-08 17:31:12 +0400

    Copyrights ©

--- HW3/src/ru/spbau/shestavin/task3/Main.java
+++ HW3/src/ru/spbau/shestavin/task3/Main.java
@@ -1,3 +1,21 @@
+/*
+* Dmitriy Shestavin
+*
+* Copyright (c) Dmitriy Shestavin & co, Inc. All Rights Reserved.
+*
+* This software is the confidential and proprietary information of
+* Dmitriy Shestavin & co, Inc. ("Confidential Information").  You shall not
+* disclose such Confidential Information and shall use it only in
+* accordance with the terms of the license agreement you entered into
+* with Dmitriy Shestavin & co, Inc.
+*
+* DMITRIY SHESTAVIN & CO MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+* THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+* TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE, OR NON-INFRINGEMENT. DMITRIY SHESTAVIN & CO SHALL NOT BE LIABLE FOR
+* ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+* DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+*/
 package ru.spbau.shestavin.task3;
 
 import ru.spbau.shestavin.task3.parsing.Parser;
@@ -18,8 +36,7 @@ public class Main {
     /**
      * Entry point of the program.
      * First command line argument is source file.
-     * Second command line argument is destination file.
-     * If second argument not present messages will be written into console.
+     * Prints result of evaluation to command line.
      *
      * @param args contains arguments from command line.
      */
--- HW3/src/ru/spbau/shestavin/task3/parsing/syntaxPrimitives/Operator.java
+++ HW3/src/ru/spbau/shestavin/task3/parsing/syntaxPrimitives/Operator.java
@@ -1,3 +1,21 @@
+/*
+* Dmitriy Shestavin
+*
+* Copyright (c) Dmitriy Shestavin & co, Inc. All Rights Reserved.
+*
+* This software is the confidential and proprietary information of
+* Dmitriy Shestavin & co, Inc. ("Confidential Information").  You shall not
+* disclose such Confidential Information and shall use it only in
+* accordance with the terms of the license agreement you entered into
+* with Dmitriy Shestavin & co, Inc.
+*
+* DMITRIY SHESTAVIN & CO MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+* THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+* TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE, OR NON-INFRINGEMENT. DMITRIY SHESTAVIN & CO SHALL NOT BE LIABLE FOR
+* ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+* DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+*/
 package ru.spbau.shestavin.task3.parsing.syntaxPrimitives;
 
 import ru.spbau.shestavin.task3.parsing.exceptions.SyntaxException;
